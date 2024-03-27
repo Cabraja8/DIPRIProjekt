e539ce63e352f901ad251b418dbb99f36fb190cd
Cabraja8 <ivancabraja0@gmail.com>
2024-03-27 17:37:09 +0100

    Added an AI that follows the player and "attacks it"

--- Assets/Scripts/EnemyScript/Enemy.cs
+++ Assets/Scripts/EnemyScript/Enemy.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 using UnityEngine.AI;
 public class Enemy : MonoBehaviour
 {
-   public Transform target; 
+    public Transform target;
+    public float speed = 5f; 
+    public float stopDistance = 2f;
 
+    private float attackTime;
+    public float attackSpeed;
+    public float TimeBetweenAttacks;
     private NavMeshAgent navMeshAgent;
 
     void Start()
@@ -15,14 +20,38 @@ public class Enemy : MonoBehaviour
         {
             Debug.LogError("Target is not set for enemy!");
         }
+        else
+        {
+            navMeshAgent.updateRotation = false;
+            navMeshAgent.stoppingDistance = stopDistance;
+        }
     }
 
     void Update()
     {
         if (target != null)
         {
-            Vector3 targetPosition = new Vector3(target.position.x, target.position.y, 0f);
-            navMeshAgent.SetDestination(targetPosition);
+            navMeshAgent.SetDestination(target.position);
+            navMeshAgent.speed = speed; 
+            if(Time.time >=attackTime){
+                StartCoroutine(Attack());
+                attackTime = Time.time + TimeBetweenAttacks;
+            }
+        }
+    }
+
+    IEnumerator Attack(){
+        
+        Vector2 originalPosition = transform.position;
+        Vector2 targetPosition = target.position;
+        float percent = 0;
+        Debug.Log("Attack");
+
+        while(percent <= 1){
+            percent += Time.deltaTime * attackSpeed;
+            float formula = (-Mathf.Pow(percent,2)+percent)*4;
+            transform.position = Vector2.Lerp(originalPosition,targetPosition,formula);
+            yield return null;
         }
     }
 }
