5135867a0b36b436d14ee395de6391f9a0bcb94f
ivanaplese <ivanaplese123@gmail.com>
2024-05-31 16:34:11 +0200

    prvi dijalog
    standField objekt

--- Assets/UIElements/DialogueTrigger.cs
+++ Assets/UIElements/DialogueTrigger.cs
@@ -6,6 +6,13 @@ public class DialogueTrigger : MonoBehaviour
 {
     public Dialogue dialogue;
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            TriggerDialogue();
+        }
+    }
     public void TriggerDialogue()
     {
         FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
