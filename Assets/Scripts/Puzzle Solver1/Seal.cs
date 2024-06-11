d2e75c096b26572045e62542bff08923d6974946
Cabraja8 <ivancabraja0@gmail.com>
2024-06-11 20:23:30 +0200

    Added Statues for Ancient ruins & Stone Puzzle Solver

---
+++ Assets/Scripts/Puzzle Solver1/Seal.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Seal : MonoBehaviour, Interactable
+{
+    public List<StoneType> requiredStoneTypes;
+    private List<StoneType> placedStoneTypes = new List<StoneType>();
+
+    public void Interact()
+    {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        // Inventory inventory = player.GetComponent<Inventory>();
+        // if (inventory != null)
+        // {
+        //     foreach (GameObject stone in inventory.stones)
+        //     {
+        //         Stone stoneScript = stone.GetComponent<Stone>();
+        //         if (stoneScript != null && requiredStoneTypes.Contains(stoneScript.stoneType))
+        //         {
+        //             placedStoneTypes.Add(stoneScript.stoneType);
+        //             inventory.stones.Remove(stone);
+        //             Destroy(stone);
+        //             break;
+        //         }
+        //     }
+
+        //     if (CheckIfPuzzleSolved())
+        //     {
+        //         OpenSeal();
+        //     }
+        // }
+    }
+
+    public bool CanInteract()
+    {
+        return true;
+    }
+
+    private bool CheckIfPuzzleSolved()
+    {
+        foreach (StoneType type in requiredStoneTypes)
+        {
+            if (!placedStoneTypes.Contains(type))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private void OpenSeal()
+    {
+        Debug.Log("Seal Opened!");
+        // Add your logic to open the seal
+    }
+}
---
+++ Assets/Scripts/Puzzle Solver1/Stone.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stone : MonoBehaviour, Interactable
+{
+    public StoneType stoneType;
+
+    public void Interact()
+    {
+        Debug.Log("Stone Interacted: " + stoneType);
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        // Inventory inventory = player.GetComponent<Inventory>();
+        // if (inventory != null)
+        // {
+        //     inventory.AddStone(gameObject);
+        //     Destroy(gameObject);
+        // }
+    }
+
+    public bool CanInteract()
+    {
+        return true;
+    }
+}
